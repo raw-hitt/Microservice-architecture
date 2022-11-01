899823661aa1870e91a8d0176f4466e69c6b09a5
rohit.pawar <rohit.pawar@hiteshi.com>
2022-11-01 12:54:37 +0530

    Add project files.

---
+++ Indore/Controllers/IndoreController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Indore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndoreController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<IndoreController> _logger;
+
+        public IndoreController(ILogger<IndoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1,1).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
